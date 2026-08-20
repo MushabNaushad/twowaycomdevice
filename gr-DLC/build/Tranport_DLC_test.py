6ed085b1a140728bb16620493388101b40922e82
maduwagepk <pabasaramaduwage@gmail.com>
2026-08-20 16:20:28 +0530

    "Working reciever ant to ant"

--- gr-DLC/build/Tranport_DLC_test.py
+++ gr-DLC/build/Tranport_DLC_test.py
@@ -5,13 +5,12 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: CRC test
+# Title: Not titled yet
 # Author: maduwage
 # GNU Radio version: 3.10.12.0
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio import DLC
 from gnuradio import blocks
 import pmt
 from gnuradio import blocks, gr
@@ -24,18 +23,17 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio import gr, pdu
-from gnuradio import pdu
+from gnuradio import transport
 import threading
 
 
 
-class crctest(gr.top_block, Qt.QWidget):
+class Tranport_DLC_test(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "CRC test", catch_exceptions=True)
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("CRC test")
+        self.setWindowTitle("Not titled yet")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -53,7 +51,7 @@ class crctest(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "crctest")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "Tranport_DLC_test")
 
         try:
             geometry = self.settings.value("geometry")
@@ -68,43 +66,31 @@ class crctest(gr.top_block, Qt.QWidget):
         ##################################################
         self.samp_rate = samp_rate = 32000
         self.flag = flag = 126
+        self.CRCpoly = CRCpoly = 0x4C11DB7
 
         ##################################################
         # Blocks
         ##################################################
 
-        self.pdu_random_pdu_0 = pdu.random_pdu(1, 2, 0xFF, 1)
-        self.pdu_pdu_to_tagged_stream_0 = pdu.pdu_to_tagged_stream(gr.types.byte_t, 'packet_len')
-        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.intern("TEST"), 10000)
-        self.blocks_message_debug_0_0_0_0 = blocks.message_debug(True, gr.log_levels.info)
-        self.blocks_message_debug_0_0_0 = blocks.message_debug(True, gr.log_levels.info)
-        self.blocks_message_debug_0_0 = blocks.message_debug(True, gr.log_levels.info)
-        self.DLC_removeFlags_0 = DLC.removeFlags(flag)
-        self.DLC_deStuffing_0 = DLC.deStuffing(126)
-        self.DLC_bitStuffing_0 = DLC.bitStuffing(126)
-        self.DLC_addFlags_0 = DLC.addFlags(flag, True)
-        self.DLC_CRCGen_0 = DLC.CRCGen(8)
-        self.DLC_CRCCheck_0 = DLC.CRCCheck(8)
+        self.transport_transport_layer_0_0 = transport.transport_layer(4, 500, "initiator", 200)
+        self.transport_transport_layer_0 = transport.transport_layer(4, 500, "initiator", 200)
+        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.cons(pmt.make_dict(), pmt.init_u8vector(25, [171, 205, 1, 0, 0, 0, 0, 17] + [ord(c) for c in "Hello from Strobe"])), 2000)
+        self.blocks_message_debug_0_1_0 = blocks.message_debug(True, gr.log_levels.info)
+        self.blocks_message_debug_0_1 = blocks.message_debug(True, gr.log_levels.info)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.DLC_CRCCheck_0, 'out'), (self.blocks_message_debug_0_0_0, 'print'))
-        self.msg_connect((self.DLC_CRCGen_0, 'out'), (self.DLC_bitStuffing_0, 'in'))
-        self.msg_connect((self.DLC_addFlags_0, 'out'), (self.pdu_pdu_to_tagged_stream_0, 'pdus'))
-        self.msg_connect((self.DLC_bitStuffing_0, 'out'), (self.DLC_addFlags_0, 'in'))
-        self.msg_connect((self.DLC_deStuffing_0, 'out'), (self.DLC_CRCCheck_0, 'in'))
-        self.msg_connect((self.DLC_deStuffing_0, 'out'), (self.blocks_message_debug_0_0, 'print'))
-        self.msg_connect((self.DLC_removeFlags_0, 'pdu_out'), (self.DLC_deStuffing_0, 'in'))
-        self.msg_connect((self.blocks_message_strobe_0, 'strobe'), (self.pdu_random_pdu_0, 'generate'))
-        self.msg_connect((self.pdu_random_pdu_0, 'pdus'), (self.DLC_CRCGen_0, 'in'))
-        self.msg_connect((self.pdu_random_pdu_0, 'pdus'), (self.blocks_message_debug_0_0_0_0, 'print'))
-        self.connect((self.pdu_pdu_to_tagged_stream_0, 0), (self.DLC_removeFlags_0, 0))
+        self.msg_connect((self.blocks_message_strobe_0, 'strobe'), (self.transport_transport_layer_0, 'app_in'))
+        self.msg_connect((self.transport_transport_layer_0, 'app_out'), (self.blocks_message_debug_0_1, 'print_pdu'))
+        self.msg_connect((self.transport_transport_layer_0, 'pdu_out'), (self.transport_transport_layer_0_0, 'pdu_in'))
+        self.msg_connect((self.transport_transport_layer_0_0, 'app_out'), (self.blocks_message_debug_0_1_0, 'print_pdu'))
+        self.msg_connect((self.transport_transport_layer_0_0, 'pdu_out'), (self.transport_transport_layer_0, 'pdu_in'))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "crctest")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "Tranport_DLC_test")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -123,10 +109,16 @@ class crctest(gr.top_block, Qt.QWidget):
     def set_flag(self, flag):
         self.flag = flag
 
+    def get_CRCpoly(self):
+        return self.CRCpoly
+
+    def set_CRCpoly(self, CRCpoly):
+        self.CRCpoly = CRCpoly
 
 
 
-def main(top_block_cls=crctest, options=None):
+
+def main(top_block_cls=Tranport_DLC_test, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
